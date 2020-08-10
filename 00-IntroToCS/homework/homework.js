3ef5a96852e0f5409611575039e2c4e4fa040507
Romano Javier <alexis_b13@live.com.ar>
2020-08-10 17:50:31 -0300

    Prueba

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -1,14 +1,15 @@
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  const arrayOfDigits = Array.from(String(num), Number);
+  
+  return arrayOfDigits;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
 }
 
-
+console.log(BinarioADecimal());
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
